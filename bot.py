beffe195bfd9e2c3f92ac4235afa53e4c04d6f0b
Piss Girl <girl@piss.com>
2021-02-28 03:34:20 +0000

    initial commit
    Signed-off-by: Piss Girl <girl@piss.com>

---
+++ bot.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+from discord.abc import PrivateChannel
+from discord.ext.commands import Bot
+import asyncio
+import random
+import re
+from vars import *
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='h-', help_command=None)
+
+general = bot.get_channel(428689943637000206)
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+    await bot.change_presence(activity=discord.Game(name="with piss"))
+
+@bot.event
+async def on_message(message):
+    if (('masspirg' in message.content) or ('MASSPIRG' in message.content) or ('Masspirg' in message.content) or ('Mass pirg' in message.content) or ('mass pirg' in message.content)):
+        await masspirg_reaction(message)
+    UMLCS = bot.get_guild(int(umlcs))
+    if (message.guild == UMLCS):
+        if ((message.channel.id == int(vergen)) or (message.channel.id == int(ephem)) or (message.channel.id == int(emo)) or (message.channel.id == int(verbot)) or (message.channel.id == int(flex))):
+            await verified_reaction(message)
+        else:
+            await nonverified_reaction(message)
+    else:
+        await otherservers_reaction(message)
+    await bot.process_commands(message)
+
+@bot.command(name='say')
+async def say(message, channelid, *, text):
+    channel = bot.get_channel(int(channelid))
+    if (message.author.id == 755142372827856946):
+        await channel.send(text)
+    else:
+        if isinstance(message.channel, discord.abc.PrivateChannel):
+            await message.channel.send('fuck you')
+
+
+@bot.command(name='ping')
+async def ping_hole(ctx):
+    if ctx.author.id == bot.user.id:
+        return
+    ping_responses = ['hole', 'pong', 'i am CLEARLY the best bot on this server', 'mrpoopybutthole', 'holes before roles!', 'don\'t tell anyone i have a crush on cubemoji', hole, 'need hole', 'h o l e'] 
+    response = random.choice(ping_responses) 
+    await ctx.send(response)
+
+
+@bot.command(name='help')
+async def help_me(message):
+    if message.author.id == bot.user.id:
+        return
+    await message.send('imagine needing help figuring out HoleBot...\nmy available commands are ``help``, ``say`` & ``ping``\nhowever don\'t use say or else')
+
+@bot.event
+async def verified_reaction(message):
+    if (holev_e.match(message.content)) or (hole in message.content):
+        await message.add_reaction(hole)
+        await message.add_reaction(h)
+        await message.add_reaction(o)
+        await message.add_reaction(l)
+        await message.add_reaction(e_emoji)
+    if (pissv_e.match(message.content)) or (pisv_e.match(message.content)):
+        piss = bot.get_emoji(int(piss_id))
+        await message.add_reaction(piss)
+    if (cumv_e.match(message.content)) or (coomv_e.match(message.content)):
+        cum = bot.get_emoji(int(cum_id))
+        await message.add_reaction(cum)
+
+@bot.event
+async def nonverified_reaction(message):
+    if (hole_e.match(message.content)) or (hole in message.content):
+        await message.add_reaction(hole)
+        await message.add_reaction(h)
+        await message.add_reaction(o)
+        await message.add_reaction(l)
+        await message.add_reaction(e_emoji)
+
+@bot.event
+async def otherservers_reaction(message):
+    if (hole_e.match(message.content)) or (hole in message.content):
+        await message.add_reaction(hole)
+        await message.add_reaction(h)
+        await message.add_reaction(o)
+        await message.add_reaction(l)
+        await message.add_reaction(e_emoji)
+    if (pissv_e.match(message.content)) or (pis_e.match(message.content)):
+        piss = bot.get_emoji(int(piss_id))
+        await message.add_reaction(piss)
+    if (cumv_e.match(message.content)) or (coom_e.match(message.content)):
+        cum = bot.get_emoji(int(cum_id))
+        await message.add_reaction(cum)
+
+@bot.event
+async def masspirg_reaction(message):
+    await message.add_reaction(f_emoji)
+    await message.add_reaction(u_emoji)
+    await message.add_reaction(c_emoji)
+    await message.add_reaction(k_emoji)
+    await message.add_reaction(m_emoji)
+    await message.add_reaction(a_emoji)
+    await message.add_reaction(s_emoji)
+    await message.add_reaction(dollar_emoji)
+    await message.add_reaction(p_emoji)
+    await message.add_reaction(i_emoji)
+    await message.add_reaction(r_emoji)
+    await message.add_reaction(g_emoji)
+
+bot.run(TOKEN)
---
+++ vars.py
@@ -0,0 +1,75 @@
+import os
+
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+from discord.abc import PrivateChannel
+from discord.ext.commands import Bot
+import asyncio
+import random
+import re
+
+hole = '\N{HOLE}'
+h = '\U0001F1ED'
+o = '\U0001F1F4'
+l = '\U0001F1F1'
+e_emoji = '\U0001F1EA'
+f_emoji = '\U0001F1EB'
+u_emoji = '\U0001F1FA'
+c_emoji = '\U0001F1E8'
+k_emoji = '\U0001F1F0'
+m_emoji = '\U0001F1F2'
+a_emoji = '\U0001F1E6'
+s_emoji = '\U0001F1F8'
+p_emoji = '\U0001F1F5'
+i_emoji = '\U0001F1EE'
+r_emoji = '\U0001F1F7'
+g_emoji = '\U0001F1EC'
+dollar_emoji = '\U0001F4B2'
+
+
+smirk = '\U0001F60F'
+holetext = "hole"
+heart = '\U0001F496'
+yellow_heart = '\U0001F49B'
+drips = '\U0001F4A6'
+
+alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+h_v = 'hHχ'
+o_v = 'oOôòóôõöøÒÓÔÕÖØü0œòȯǒōõǫőốồṓṑȱṍȫỗṏǿȭǭỏơổọớờỡộởợ'
+o_vv = 'oOyYôòóôõöøÒÓÔÕÖØü0œòȯǒōõǫőốồṓṑȱṍȫỗṏǿȭǭỏơổọớờỡộởợ'
+l_v = 'lL1ḹḻḽλ'
+e_v = 'eEëèéêËÈÊÉ3ėěĕēẽęȩɇếềḗḕễḝẻȅȇểẹḙḛệεⒺⓔ⒠'
+p_v = 'PpπⓅⓟ⒫℗ṔṕṖṗƤƥⱣℙǷꟼ℘'
+i_v = 'iI!1|ïÍÍíi̇́Ììi̇̀ĬĭÎîǏǐÏïḮḯĨĩi̇̃ĮįĮ́į̇́Į̃į̇̃ĪīĪ̀ī̀ỈỉȈȉI̋i̋ȊȋỊḬḭƗɨİiIıⒾⓘ⒤IiḬḭḮḯÍíÌìÎîÏïĨĩĪīĬĭĮįǏǐıƚⅈℹ'
+s_v = 'sS$5zΣšßŠς'
+double_s = 'ß'
+c_v = 'ckKCĆćĈĉČčĊċC̄c̄ÇçḈḉȻȼƇƈɕƇ₡₢Ⓒⓒ⒞'
+u_v = 'uUūŪúǔǓùÙŭŬûÛüÜůŮųŲũŨűŰȕȔṳṲṵṴṷṶṹṸṻṺ'
+m_v = 'mMⓂⓜ⒨ḾḿṀṁṂṃꟿꟽⱮƜℳ'
+
+hole_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+h_v+']+([\s\n]*)+['+o_v+']+([\s\n]*)+['+l_v+']+([\s\n]*)+['+e_v+']+.*', re.MULTILINE)
+piss_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+p_v+']+([\s\n]*)+['+i_v+']+([\s\n]*)+['+s_v+']+([\s\n]*)+['+s_v+']+.*', re.MULTILINE)
+pis_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+p_v+']+([\s]*)+['+i_v+']+([\s\n]*)+['+double_s+']+.*', re.MULTILINE)
+pee_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+p_v+']+([\s]*)+['+e_v+']+([\s\n]*)+['+e_v+']+(?<!['+alphabet+'])+.*', re.MULTILINE)
+cum_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+c_v+']+([\s\n]*)+['+u_v+']+([\s\n]*)+['+m_v+']+.*', re.MULTILINE)
+coom_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+c_v+']+([\s\n]*)+['+o_vv+']+([\s\n]*)+['+o_vv+']+([\s\n]*)+['+m_v+']+.*', re.MULTILINE)
+
+holev_e = re.compile(r'.*(?<![wWcC])+([\n\s]*)+['+h_v+']+([\s\n]*)+['+o_v+']+([\s\n]*)+['+l_v+']+([\s\n]*)+['+e_v+']+.*', re.MULTILINE)
+pissv_e = re.compile(r'.*([\n\s]*)+['+p_v+']+([\s\n]*)+['+i_v+']+([\s\n]*)+['+s_v+']+([\s\n]*)+['+s_v+']+.*', re.MULTILINE)
+pisv_e = re.compile(r'.*([\n\s]*)+['+p_v+']+([\s]*)+['+i_v+']+([\s\n]*)+['+double_s+']+.*', re.MULTILINE)
+peev_e = re.compile(r'.*(?<!['+alphabet+'])+([\n\s]*)+['+p_v+']+([\s]*)+['+e_v+']+([\s\n]*)+['+e_v+']+(?<!['+alphabet+'])+.*', re.MULTILINE)
+cumv_e = re.compile(r'.*([\n\s]*)+['+c_v+']+([\s\n]*)+['+u_v+']+([\s\n]*)+['+m_v+']+.*', re.MULTILINE)
+coomv_e = re.compile(r'.*([\n\s]*)+['+c_v+']+([\s\n]*)+['+o_vv+']+([\s\n]*)+['+o_vv+']+([\s\n]*)+['+m_v+']+.*', re.MULTILINE)
+
+vergen = 803366782819237918
+ephem = 803366987581095936 
+emo = 803367390669701171
+verbot = 806533217850884096
+flex = 803367576599920640
+umlcs = 428689943637000202
+
+piss_id = 807400993246412821
+cum_id = 807348408296800276
